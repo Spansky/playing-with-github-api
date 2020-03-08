53e8a4fbc5ed982a5c07d1f2a5998eda630b6657
Leon Sczepansky <leon.sczepansky@gmail.com>
2020-03-08 11:49:50 +0800

    updated last script

--- githubjobs.py
+++ githubjobs.py
@@ -3,7 +3,7 @@ dev_count=0
 other_count=0
 for job in requests.get("https://jobs.github.com/positions.json?utf8=%E2%9C%93&description=&location=New+York").json():
     print("Type: {0}, Description: {1}".format(job['type'], job['title']))
-    if ("Developer" or "Engineer") in job['title']:
+    if ("Developer" in job['title']) or ("Engineer" in job['title']):
         dev_count+=1
     else:
         other_count+=1
